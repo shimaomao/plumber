02850e9d8a1ffb141f936043a03268f1e00fee6b
hejiaxuan7 <hejiaxuan7@sina.com>
2019-08-19 16:51:55 +0800

    删掉管理平台

--- plumber-admin/src/main/resources/static/admin/index.js
+++
@@ -1,7 +0,0 @@
-layui.extend({
-    admin: 'lay/modules/admin',
-    validate: 'lay/modules/validate'
-}).define(['admin', 'conf'], function (exports) {
-    layui.admin.initPage();
-    exports('index', {});
-});
